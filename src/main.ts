10118b65044e6839939cee6e5d979aeda6f11218
Fahad Waseem <fahad.waseem@da1realtech.com>
2024-10-27 15:49:18 +0500

    enabled validation pipes and removed useless console.logs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
