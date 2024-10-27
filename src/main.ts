ab47a936678c20158f85deb71ad09acc879e3861
Fahad Waseem <fahad.waseem@da1realtech.com>
2024-10-27 15:16:03 +0500

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { hostname } from 'os';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
